2eea126978c0aa1f128d5440a368fceb8f671ee2
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-08-24 11:30:47 -0600

    Uploading Template

---
+++ lab12-02.c
@@ -0,0 +1,13 @@
+/** lab12-02.c
+ * =========================================================== 
+ * Name: name, date
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
+
+int main() {
+    
+    return 0;
+}
