15b3ad865ae1872ca213e1d60e933096bbda0958
Jason McGinthy <jasonmcginthy@Jasons-Laptop.local>
2024-09-11 14:46:31 -0600

    updates f24

--- lab12functs.h
+++ lab12functs.h
@@ -6,12 +6,3 @@
  * Purpose: high level description of purpose of the program
  *          could be multiple lines
  * =========================================================== */
-
-#ifndef LAB12FUNCTS_H
-#define LAB12FUNCTS_H
-
-int numFactorial(int n);
-int isPrime(int n);
-void floydsTriangle(int rows);
-
-#endif
